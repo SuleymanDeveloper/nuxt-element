17da5a3707959bdb0179866e9f9c8f01c32cffcd
Suleyman <suleyman01acc@gmail.com>
2022-05-16 23:28:28 +0500

    nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,60 @@
+import { defineNuxtConfig } from 'nuxt'
+
+const lifecycle = process.env.npm_lifecycle_event
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  // meta
+  meta: {
+    title: 'Element Plus + Nuxt 3',
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'ElementPlus + Nuxt3',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // css
+  css: ['~/assets/scss/index.scss'],
+
+  // build
+  build: {
+    transpile:
+      lifecycle === 'build' || lifecycle === 'generate' ? ['element-plus'] : [],
+  },
+
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+
+  // build modules
+  modules: ['@vueuse/nuxt', '@unocss/nuxt', '@pinia/nuxt'],
+
+  // auto import components
+  components: true,
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  unocss: {
+    uno: true,
+    attributify: true,
+    // preflight: true,
+    icons: {
+      scale: 1.2,
+    },
+    shortcuts: [
+      [
+        'btn',
+        'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
+      ],
+    ],
+  },
+})
